f811964805919a3ac1d4c114813f4a50e562cb80
Tales Takemiya <tales.takemiya@gmail.com>
2025-01-30 06:46:33 -0300

    Adding sha-256 encoding class

---
+++ crypto_hash.py
@@ -0,0 +1,18 @@
+import hashlib
+import json
+
+def crypto_hash(data):
+    """
+    Return a sha-256 hash of the given data
+    """
+    stringified_data = json.dumps(data)
+    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+
+def main():
+    print(f"crypto_hash('foo'): {crypto_hash('foo')}")
+    print(f"crypto_hash(1): {crypto_hash(1)}")
+    print(f"crypto_hash([2, 3]): {crypto_hash([2, 3])}")
+
+
+if __name__ == '__main__':
+    main()
