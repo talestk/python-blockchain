1d731db3b33b5c835daf9a18152268aef6f67aa9
Tales Takemiya <tales.takemiya@gmail.com>
2025-01-30 06:24:20 -0300

    Building the block and genesis block

--- blockchain.py
+++ blockchain.py
@@ -6,6 +6,9 @@ class Block:
     def __init__(self, data):
         self.data = data
 
+    def __repr__(self):
+        return f'Block - data: {self.data}'
+
 class Blockchain:
     """
     Blockchain: a public ledger of transactions.
@@ -14,4 +17,19 @@ class Blockchain:
     
     def __init__(self):
         self.chain = []
+
+    def add_block(self, data):
+        self.chain.append(Block(data))
+
+    def __repr__(self):
+        return f'Blockchain: {self.chain}'
+
+    
+
+
+blockchain = Blockchain()
+blockchain.add_block('one')
+blockchain.add_block('two')
+
+print(blockchain)
 
