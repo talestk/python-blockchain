cc035081c3d648d2d0676709e0d8dee0afe537ae
Tales Takemiya <tales.takemiya@gmail.com>
2025-01-30 06:24:20 -0300

    Building the block and genesis block

---
+++ block.py
@@ -0,0 +1,48 @@
+import time
+
+
+def mine_block(last_block, data):
+    """
+    Mine a block on the given last_block and data.
+    """
+    timestamp = time.time_ns()
+    last_hash = last_block.hash
+    hash = f'{timestamp}-{last_hash}'
+
+    return Block(timestamp, last_hash, hash, data)
+
+
+def genesis():
+    """
+    Generate the genesis block.
+    """
+    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
+
+
+class Block:
+    """
+    Block: a unit of storage
+    Store transactions in a blockchain that supports a crypto currency.
+    """
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
+        self.data = data
+
+    def __repr__(self):
+        return (
+            f'timestamp: {self.timestamp}, '
+            f'last_hash: {self.last_hash}, '
+            f'hash: {self.hash}, '
+            f'data: {self.data}' 
+        )
+    
+def main():
+    genesis_block = genesis()
+    block = mine_block(genesis_block, 'foo')
+    print(block)
+
+if __name__ == '__main__':
+    main()
