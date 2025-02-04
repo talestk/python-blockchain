19ff4342f4b4a749e9ee6549af26d96ac8cf8b4f
Tales Takemiya <tales.takemiya@gmail.com>
2025-02-04 08:23:08 -0300

    Blockchain API+Flask first steps

--- README.md
+++ README.md
@@ -11,7 +11,13 @@ pip install -r requirements.txt
 **Run the tests**
 
 Make sure to activate the virtual environment.
-
 ```
 python3 -m pytest backend/tests
+```
+
+**Run the application and API**
+
+Make sure to activate the virtual environment.
+```
+python -m backend.app
 ```
---
+++ backend/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+from backend.blockchain.blockchain import Blockchain
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+for i in range(3):
+    blockchain.add_block(i)
+    
+
+@app.route('/')
+def reoute_default():
+    return 'Welcome to the blockchain'
+
+@app.route('/blockchain')
+def route_blockchain():
+    return jsonify(blockchain.to_json())
+
+
+app.run(port=5001)
+
