228c5798e073d878abe639f4a8b8d603edebb1b3
Tales Takemiya <tales.takemiya@gmail.com>
2025-02-05 16:43:06 -0300

    Removing redundacy and Synchronize a Peer on startup

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -4,6 +4,8 @@ import os
 from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
+from backend.blockchain.block import Block
+
 
 # we need to get the keys from a file for safety
 # do not add the file to github
@@ -23,8 +25,23 @@ CHANNELS = {
 }
 
 class Listener(SubscribeCallback):
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
+
+
     def message(self, pubnub, message_object):
         print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
+
+        if message_object.channel == CHANNELS['BLOCK']:
+            block = Block.from_json(message_object.message)
+            potential_chain = self.blockchain.chain[:]
+            potential_chain.append(block)
+
+            try:
+                self.blockchain.replace_chain(potential_chain)
+                print(f'\n -- Successfully replaced the local chain')
+            except Exception as e:
+                print(f'\n -- Did not replace chain: {e}')
 
 
 class PubSub():
@@ -33,16 +50,19 @@ class PubSub():
     Provides communication between the nodes of the blockchain network.
     """
 
-    def __init__(self):
+    def __init__(self, blockchain):
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener())
+        self.pubnub.add_listener(Listener(blockchain))
 
     def publish(self, channel, message):
         """
         Publish the message object to the channel.
         """
+        self.pubnub.unsubscribe().channels([channel]).execute()
         self.pubnub.publish().channel(channel).message(message).sync()
+        self.pubnub.subscribe().channels([channel]).execute()
+        #self.pubnub.publish().channel(channel).message(message).sync()
 
     def broadcast_block(self, block):
         """
