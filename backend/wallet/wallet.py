a7a4098a63fd6c92f553fbbcd2a2432a78943038
Tales Takemiya <tales.takemiya@gmail.com>
2025-02-07 08:05:33 -0300

    Signing data

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,8 +1,10 @@
+import json
 import uuid
 
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
 
 class Wallet:
     """
@@ -19,10 +21,22 @@ class Wallet:
             default_backend())
         self.public_key = self.private_key.public_key()
 
+    def sign(self, data):
+        """
+        Generate a signature based on the data using the local private key.
+        """
+        return self.private_key.sign(
+            json.dumps(data).encode('utf-8'),
+            ec.ECDSA(hashes.SHA256()))
+
 
 def main():
     wallet = Wallet()
     print(f'wallet.__dict__: {wallet.__dict__}')
+
+    data = {'foo' : 'bar'}
+    signature = wallet.sign(data)
+    print(f'signature: {signature}')
 
 if __name__ == '__main__':
     main()
