1273b2e86678b3c95d103b0e18530def739fe08d
Tales Takemiya <tales.takemiya@gmail.com>
2025-02-12 06:57:41 -0300

    Transaction pool

---
+++ backend/wallet/transaction_pool.py
@@ -0,0 +1,11 @@
+from backend.wallet.transaction import Transaction
+
+class TransactionPool:
+    def __init__(self):
+        self.transaction_map = {}
+
+    def set_transaction(self, transaction):
+        """
+        Set a transaction in the transaction pool.
+        """
+        self.transaction_map[transaction.id] = transaction
