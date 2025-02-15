fe41f84e832ba842f9b1590d0149525b62363875
Tales Takemiya <tales.takemiya@gmail.com>
2025-02-15 08:27:09 -0300

    Clear transaction pool

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -41,6 +41,7 @@ class Listener(SubscribeCallback):
 
             try:
                 self.blockchain.replace_chain(potential_chain)
+                self.transaction_pool.clear_blockchain_transactions(self.blockchain)
                 print(f'\n -- Successfully replaced the local chain')
             except Exception as e:
                 print(f'\n -- Did not replace chain: {e}')
