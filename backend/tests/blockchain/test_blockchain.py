5e9baf4cdf2a82376dc8de63760a7a5bce9a076c
Tales Takemiya <tales.takemiya@gmail.com>
2025-02-20 08:50:36 -0300

    Transaction chain validations

--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -1,6 +1,8 @@
 import pytest
 
 from backend.blockchain.blockchain import Blockchain
+from backend.wallet.transaction import Transaction
+from backend.wallet.wallet import Wallet
 from backend.blockchain.block import GENESIS_DATA
 
 def test_blockchain_instance():
@@ -17,38 +19,67 @@ def test_add_block():
     assert blockchain.chain[-1].data == data
 
 @pytest.fixture
-def block_chain_three_blocks():
+def blockchain_three_blocks():
     blockchain = Blockchain()
     
     for i in range(3):
-        blockchain.add_block(i)
+        blockchain.add_block([Transaction(Wallet(), 'recipient', i).to_json()])
     
     return blockchain
 
-def test_is_valid_chain(block_chain_three_blocks):
-    Blockchain.is_valid_chain(block_chain_three_blocks.chain)
+def test_is_valid_chain(blockchain_three_blocks):
+    Blockchain.is_valid_chain(blockchain_three_blocks.chain)
 
-def test_is_valid_chain_bad_genesis(block_chain_three_blocks):
-    block_chain_three_blocks.chain[0].hash = 'evil _hash'
+def test_is_valid_chain_bad_genesis(blockchain_three_blocks):
+    blockchain_three_blocks.chain[0].hash = 'evil _hash'
 
     with pytest.raises(Exception, match='genesis block must be valid'):
-        Blockchain.is_valid_chain(block_chain_three_blocks.chain)
+        Blockchain.is_valid_chain(blockchain_three_blocks.chain)
 
-def test_replace_chain(block_chain_three_blocks):
+def test_replace_chain(blockchain_three_blocks):
     blockchain = Blockchain()
-    blockchain.replace_chain(block_chain_three_blocks.chain)
+    blockchain.replace_chain(blockchain_three_blocks.chain)
 
-    assert blockchain.chain == block_chain_three_blocks.chain
+    assert blockchain.chain == blockchain_three_blocks.chain
 
-def test_replace_chain_not_longer(block_chain_three_blocks):
+def test_replace_chain_not_longer(blockchain_three_blocks):
     blockchain = Blockchain()
 
     with pytest.raises(Exception, match='The incoming chain must be longer'):
-        block_chain_three_blocks.replace_chain(blockchain.chain)
+        blockchain_three_blocks.replace_chain(blockchain.chain)
 
-def est_replace_chain_bad_chain(block_chain_three_blocks):
+def test_replace_chain_bad_chain(blockchain_three_blocks):
     blockchain = Blockchain()
-    block_chain_three_blocks[1].hash = 'evil_hash'
+    blockchain_three_blocks.chain[1].hash = 'evil_hash'
 
     with pytest.raises(Exception, match='The incoming chain is invalid'):
-        blockchain.replace_chain(block_chain_three_blocks.chain)
+        blockchain.replace_chain(blockchain_three_blocks.chain)
+
+def test_valid_transaction_chain(blockchain_three_blocks):
+    Blockchain.is_valid_transaction_chain(blockchain_three_blocks.chain)
+
+def test_is_valid_transaction_chain_duplicate_transactions(blockchain_three_blocks):
+    transaction = Transaction(Wallet(), 'recipient', 1).to_json()
+
+    blockchain_three_blocks.add_block([transaction, transaction])
+    
+    with pytest.raises(Exception, match='is not unique'):
+        Blockchain.is_valid_transaction_chain(blockchain_three_blocks.chain)
+
+def test_is_valid_transaction_chain_multiple_rewards(blockchain_three_blocks):
+    reward_1 = Transaction.reward_transaction(Wallet()).to_json()
+    reward_2 = Transaction.reward_transaction(Wallet()).to_json()
+
+    blockchain_three_blocks.add_block([reward_1, reward_2])
+        
+    with pytest.raises(Exception, match='one mining reward per block'):
+        Blockchain.is_valid_transaction_chain(blockchain_three_blocks.chain)
+
+def test_is_valid_transaction_chain_bad_transaction(blockchain_three_blocks):
+    bad_transaction = Transaction(Wallet(), 'recipient', 1)
+    bad_transaction.input['signature'] = Wallet().sign(bad_transaction.output)
+    blockchain_three_blocks.add_block([bad_transaction.to_json()])
+
+    with pytest.raises(Exception):
+        Blockchain.is_valid_transaction_chain(blockchain_three_blocks.chain)
+
