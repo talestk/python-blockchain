521acc2fdcbd8d2f94efdd202c7beba9a98a040d
Tales Takemiya <tales.takemiya@gmail.com>
2025-03-27 09:50:56 -0300

    Conduct Transaction component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import './index.css';
 import App from './components/App';
+import Blockchain from './components/Blockchain';
+import ConductTransaction from './components/ConductTransaction';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
